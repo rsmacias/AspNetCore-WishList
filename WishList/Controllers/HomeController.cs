7fb9301602befe36b29a24d3e9b88d8f8b83baa6
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 18:33:45 -0500

    Workarounds to pass pluralsight's validations 2

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,15 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 
-public class HomeController: Controller {
-    public HomeController() {
-        
-    }
+namespace WishList.Controllers {
+    public class HomeController: Controller {
+        public HomeController() {
+            
+        }
 
-    public IActionResult Index() {
-        return View("Index");
-    }
+        public IActionResult Index() {
+            return View("Index");
+        }
 
-    public IActionResult Error() {
-        return View("Error");
+        public IActionResult Error() {
+            return View("Error");
+        }
     }
 }
