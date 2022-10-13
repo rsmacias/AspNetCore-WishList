167d1828a086b4142bfb1485c6f7498cd8dc36de
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 17:54:41 -0500

    Add UseEndpoints in Configure

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,6 +24,9 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
