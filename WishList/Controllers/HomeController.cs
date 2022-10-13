9939d0da5b45071190b23270b59edd565fe71ea7
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 18:15:53 -0500

    > Create the HomeController > Create HomeController's Index Action

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController: Controller {
+    public HomeController() {
+        
+    }
+
+    public IActionResult Index() {
+        return View();
+    }
+}
