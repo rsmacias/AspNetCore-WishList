81f662376318cf8fcb06f61cf3b89c3642596cf6
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 18:19:22 -0500

    Create HomeController's Error Action

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -8,4 +8,8 @@ public class HomeController: Controller {
     public IActionResult Index() {
         return View();
     }
+
+    public IActionResult Error() {
+        return View("Error");
+    }
 }
