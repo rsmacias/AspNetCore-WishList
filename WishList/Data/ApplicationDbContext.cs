1eba7149b54d1f6354cf9bf009f9ae0d45d3b396
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 18:58:53 -0500

    Create Item Model > Add Item to ApplicationDbContext

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,7 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
-public class ApplicationDbContext : DbContext {
-    public ApplicationDbContext(DbContextOptions options) : base(options){
-        
+namespace WishList.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions options) : base(options){
+            
+        }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
