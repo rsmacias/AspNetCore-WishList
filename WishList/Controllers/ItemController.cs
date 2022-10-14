f3de4d63510c2d95d310500e9752c47e353ec204
Robert Macias <Robert.Macias@trialcard.com>
2022-10-13 20:23:45 -0500

    Create The ItemController > Create ItemController > Create ItemController's Index Action > Create ItemController's HttpGet Create Action > Create ItemController's HttpPost Create Action > Create ItemController's Delete Action

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers {
+    public class ItemController : Controller {
+
+        private readonly ApplicationDbContext _context = null;
+
+        public ItemController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index(){
+            var wishes = _context.Items.ToList();
+            return View("Index", wishes);
+        }
+
+        [HttpGet]
+        public IActionResult Create(){
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item){
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id){
+            Item itemFound = _context.Items.FirstOrDefault( item => item.Id == id);
+            _context.Items.Remove(itemFound);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
